4fbb672baf1748663c47d154342fe06a7cae6f84
Paul Henshaw <paul+epg@openquake.org>
2015-05-05 00:06:53 +0200

    Split into a few functions, pyliny driven cleanup, added description support (still too many locals), Handle different Rai 3 names

--- CnPEPG.py
+++ CnPEPG.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-#
+"""
 # Legge JSON file creato da cutandpasta.it epg.xml e aggiorna DB sqlite
 # Vedi anche
 # http://www.cutandpasta.it
-#
+"""
 
 import sys
 import datetime
@@ -13,133 +13,137 @@ import json
 
 from unidecode import unidecode
 
-infil = sys.argv[1]
-
-with open(infil) as data_file:
-    js = json.load(data_file)
-
-bc = js["tv"].get("programme")
-
-if not bc:
-    print "Nessun Programma Disponibile da cutandpasta.it"
-    sys.exit(1)
-
-conn = sqlite3.connect('epg.v2.sqlite')
-c = conn.cursor()
-
-for bc in js["tv"]["programme"]:
-    cn = bc["@channel"]
-
-    # Handle inconsistent spelling of channel names
-    if cn == u'Rete 4':
-        channel_name = u'Rete4'
-    elif cn == u'Canale 5':
-        channel_name = u'Canale5'
-    elif cn == u'Italia 1':
-        channel_name = u'Italia1'
-    elif cn == u'La 5':
-        channel_name = u'La5'
-    elif cn == u'La7d':
-        channel_name = u'La7D'
-    elif cn == u'Rai Sport1':
-        channel_name = u'Rai Sport 1'
-    elif cn == u'Rai Sport2':
-        channel_name = u'Rai Sport 2'
+def _channel_name(name):
+    """
+    Handle inconsistent spelling of channel names
+    """
+    if name == u'Rete 4':
+        newname = u'Rete4'
+    elif name == u'Canale 5':
+        newname = u'Canale5'
+    elif name == u'Italia 1':
+        newname = u'Italia1'
+    elif name == u'La 5':
+        newname = u'La5'
+    elif name == u'La7d':
+        newname = u'La7D'
+    elif name == u'Rai Sport1':
+        newname = u'Rai Sport 1'
+    elif name == u'Rai Sport2':
+        newname = u'Rai Sport 2'
+    elif name[0:9] == u'Rai 3 TGR':
+        newname = u'Rai 3'
     else:
-        channel_name = cn
+        newname = name
+    return newname
 
-    # Load existing channel data from DB
-    p = (channel_name,)
-    c.execute('SELECT id FROM channel WHERE name = ?', p)
-    row = c.fetchone()
-    if not row:
-        print "Ignoring unrecognised channel  \"{0}\" ".format(channel_name)
-        continue
-    channel = row[0]
+def _mins_since_epoch(datestring):
+    """
+    Minutes since the Epoch from a given date string
+    """
+    return int(datetime.datetime.strptime(datestring.split('+')[0],
+            '%Y%m%d%H%M%S ').strftime("%s")) / 60
 
-    # Get start/end dates
-    pstart = bc["@start"]
-    start_date = int(datetime.datetime.strptime(pstart.split('+')[0],
-        '%Y%m%d%H%M%S ').strftime("%s"))
-    start_date = start_date / 60
-    pend = bc["@stop"]
-    end_date = int(datetime.datetime.strptime(pend.split('+')[0],
-        '%Y%m%d%H%M%S ').strftime("%s"))
-    end_date = end_date / 60
-
-    p = (channel, end_date)
-    c.execute('SELECT id FROM show WHERE channel = ? AND end_date= ?', p)
-    row = c.fetchone()
-    if row:
-        #print 'Entry for show already present, skipping...'
-        continue
-
-    # Calculate duration, ignore empty or negative duration entries
-    duration = end_date - start_date
-    if duration <= 0:
-        print 'Ignoring entry with bad duration {0}'.format(
-            (channel_name, start_date, end_date, duration))
-        continue
-    # Generate unique ID from channel + timestamp
-    pid = end_date + 100000000 * channel
-
-    ptype = None
-
-    title = bc["title"]
+def _text(title):
+    """
+    Extract text from tag element - if there are multiple elements, use first
+    """
     if isinstance(title, dict):
-        line = title["#text"]
+        text = title["#text"]
     elif isinstance(title, list):
         # If there are multiple titles, take first
         # - a better plan would be to check language codes
-        line = title[0]["#text"]
+        text = title[0]["#text"]
     else:
-        print "Unnexpected Title type: \
-        Channel={0} Start={1} title={2} class={3}".format(
-                channel_name, start_date, title, title.__class__)
-        line = str(title)
+        #        print "Unnexpected Title type: \
+        #        Channel={0} Start={1} title={2} class={3}".format(
+        #                channel_name, start_date, title, title.__class__)
+        text = str(title)
+    return text
 
-    if line:
-        title = unidecode(line)
-        if title[0:4] == 'dvd ':
-            title = title[4:]
-            ptype = 6 # FILM
-    else:
-        title = 'Titolo Non Disponibile'
+def main():
+    """
+    Parse JSON file, update SQLIte DB
+    """
+    filename = sys.argv[1]
 
+    with open(filename) as data_file:
+        jsondata = json.load(data_file)
 
-    subtitle = None
-    description = None
-    year = None
-    country = None
-    bid = None
-    parental = None
-    serie = None
-    replica = None
+    prog = jsondata["tv"].get("programme")
 
-    is_premiere = 0
-    is_hd = 0
-    is_premium = 0
-    is_live = 0
-    is_lis = 0
-    is_subtitled = 0
+    if not prog:
+        print "Nessun Programma Disponibile da cutandpasta.it"
+        sys.exit(1)
 
-    subtitle_page = None
-    imdb_id = None
-    twitter = None
-    season = None
-    episode = None
-    thumbnail_url = None
+    conn = sqlite3.connect('epg.v2.sqlite')
+    cursor = conn.cursor()
 
-    p = (pid, channel, end_date, title, subtitle, serie, duration, ptype)
-    #print p
-    print unidecode(channel_name), end_date, title, duration
+    for prog in jsondata["tv"]["programme"]:
+        channel_name = _channel_name(prog["@channel"])
 
-    c.execute('INSERT INTO show(id,channel,end_date,title,\
-    subtitle,serie,duration,type) \
-    VALUES(?,?,?,?,?,?,?,?)', p)
+        # Load existing channel data from DB
+        params = (channel_name,)
+        cursor.execute('SELECT id FROM channel WHERE name = ?', params)
+        row = cursor.fetchone()
+        if not row:
+            print "Ignoring unrecognised channel  \"{0}\" ".format(channel_name)
+            continue
+        channel = row[0]
 
-conn.commit()
-conn.close()
+        # Get start/end dates
+        start_date = _mins_since_epoch(prog["@start"])
+        end_date = _mins_since_epoch(prog["@stop"])
+
+        params = (channel, end_date)
+        cursor.execute('SELECT id FROM show WHERE channel = ? AND end_date= ?',
+            params)
+        row = cursor.fetchone()
+        if row:
+            #print 'Entry for show already present, skipping...'
+            continue
+
+        # Calculate duration, ignore empty or negative duration entries
+        duration = end_date - start_date
+        if duration <= 0:
+            print 'Ignoring entry with bad duration {0}'.format(
+                (channel_name, start_date, end_date, duration))
+            continue
+
+        # Generate unique ID from channel + timestamp
+        pid = end_date + 100000000 * channel
+
+        ptype = None
+
+        line = _text(prog["title"])
+        if line:
+            title = unidecode(line)
+            if title[0:4] == 'dvd ':
+                title = title[4:]
+                ptype = 6 # FILM
+        else:
+            title = 'Titolo Non Disponibile'
+
+        description = None
+        if prog.get("desc"):
+            description = unidecode(_text(prog["desc"]))
+            print "** Desc \'{0}\'".format(description)
+
+        params = (pid, channel, end_date, title, duration, ptype, description)
+        #print params
+        print unidecode(channel_name), end_date, title, duration
+
+        cursor.execute('INSERT INTO show(id,channel,end_date,title,\
+        duration,type,description) \
+        VALUES(?,?,?,?,?,?,?)', params)
+
+    conn.commit()
+    conn.close()
+
+#
+# Main driver
+#
+if __name__ == "__main__":
+    main()
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-
