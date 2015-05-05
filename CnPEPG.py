4b2844b6217db18057a9391ddccc42c3c9aba661
Alberto 'JCN-9000' <jcn9000@gmail.com>
2015-05-05 20:02:05 +0200

    Handle categorization
    Just an initial coding, branching until verified

--- CnPEPG.py
+++ CnPEPG.py
@@ -13,6 +13,109 @@ import json
 
 from unidecode import unidecode
 
+def _type(scat):
+    """
+    Handle categorization
+    Needs some reworking on case management ( upper, lower, title ... )
+    """
+
+    xml_types = {
+    'Altri Eventi Sportivi': 7,
+    'Animazioni': 16,
+    'Approfondimento': 0,
+    'Attualita\'': 6,
+    'Avventura' : 0,
+    'Azione' : 0,
+    'Biografico' : 0,
+    'Cartone animato' : 16,
+    'Cartoon' : 16,
+    'Comico' : 0,
+    'Commedia' : 0,
+    'Concerto' : 17,
+    'Cucina e Sapori' : 29,
+    'Cultura' : 24,
+    'Dibattito' : 0,
+    'Di Montaggio' : 0,
+    'Divulgazione Scientifico/Culturale' : 0,
+    'Docu-Fiction' : 0,
+    'Documentari' : 12,
+    'Documentario' : 12,
+    'Documentaristico' : 12,
+    'Docu-Soap' : 15,
+    'Docutainment' : 12,
+    'Drammatico' : 0,
+    'Fantascienza' : 0,
+    'Fantastico/Favolistico' : 0,
+    'Fiction' : 0,
+    'Film' : 6,
+    'Film TV' : 6,
+    'Funzione Religiosa' : 25,
+    'Game show' : 14,
+    'Game Show/Quiz' : 14,
+    'Giallo' : 0,
+    'Guerra' : 0,
+    'Horror' : 0,
+    'Intrattenimento' : 3,
+    'Manifestazione Sportiva' : 7,
+    'Mondo e tendenze' : 4,
+    'Musica' : 17,
+    'Natura/Ambiente/Etnologia' : 0,
+    'News' : 2,
+    'Novelas' : 8,
+    'Poliziesco' : 0,
+    'Programma di servizio' : 0,
+    'Programma musicale' : 17,
+    'Programmi culturali' : 24,
+    'Programmi religiosi' : 25,
+    'Reality' : 19,
+    'Real TV' : 19,
+    'Religione' : 25,
+    'Riassunto telenovelas' : 8,
+    'Rotocalco' : 0,
+    'Rubrica attualita\' sportiva' : 7,
+    'Rubrica autopromozionale' : 0,
+    'Rubrica di attualita\'' : 2,
+    'Rubrica di servizio' : 0,
+    'Salute' : 26,
+    'Sentimentale' : 0,
+    'Serie' : 9,
+    'Shopping' : 21,
+    'Sit-com' : 22,
+    'Sitcom' : 22,
+    'Soap' : 15,
+    'Soap opera' : 15,
+    'Spionaggio' : 20,
+    'Sport' : 7,
+    'Storico' : 0,
+    'Talk show' : 11,
+    'Telefilm' : 9,
+    'Telenovela' : 8,
+    'Telegiornale' : 2,
+    'Telegiornale sportivo' : 2,
+    'Telegiornali' : 2,
+    'Thriller' : 0,
+    'Turismo/geografia/etnologia' : 0,
+    'Varieta\'' : 0,
+    'Western' : 0,
+    }
+
+    typ=None
+
+    if scat:
+        #~ print scat
+        if isinstance(scat,list):
+            cat = scat[0].get("#text")
+        else:
+            cat = scat.get("#text")
+        cat=cat.split('<')[0]
+
+        xtyp = xml_types.get(cat.title())
+        if xtyp:
+            typ=xtyp
+
+        #~ print typ, cat
+    return typ
+
 def _channel_name(name):
     """
     Handle inconsistent spelling of channel names
@@ -131,6 +234,11 @@ def main():
                 ptype = 6
         else:
             title = 'Titolo Non Disponibile'
+        
+        typ = _type(prog.get("category"))
+        if typ:
+            if not ptype:
+                ptype = typ
 
         description = None
         if prog.get("desc"):
