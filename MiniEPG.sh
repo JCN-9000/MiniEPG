ad98e9632323fe60779b6bfe7dcfadd3855c479d
Alberto 'JCN-9000' <jcn9000@gmail.com>
2015-06-03 17:56:50 +0200

    Define PYTHON command

--- MiniEPG.sh
+++ MiniEPG.sh
@@ -17,6 +17,7 @@ ZIP='/usr/bin/zip'
 ZIP_OPT='-u'
 WGET_OPT='-q $PU $PP'
 SQLITE3='/usr/bin/sqlite3'
+PYTHON='python'
 
 DO_TVB=0
 DO_CNP=0
@@ -41,17 +42,17 @@ then
 # Download http://epgadmin.tvblob.com/api/services
 # Loop in keys and get EPG
     [ -f services.xml ] || wget $WGET_OPT -O services.xml http://epgadmin.tvblob.com/api/services
-    python xml2json.py -t xml2json --pretty -o services.json services.xml
+    $PYTHON xml2json.py -t xml2json --pretty -o services.json services.xml
 
     grep key services.json | tr -d '",' | while read Key Channel
     do
 #    echo $Channel
         wget $WGET_OPT -O $Channel.0.xml http://epgadmin.tvblob.com/api/$Channel/programmes/schedules/today
-        [ -f $Channel.0.xml  ] && python xml2json.py -t xml2json -o $Channel.0.json $Channel.0.xml && rm $Channel.0.xml
-        [ -f $Channel.0.json ] && python TVBLOB_EPG.py $Channel.0.json ; rm $Channel.0.json
+        [ -f $Channel.0.xml  ] && $PYTHON xml2json.py -t xml2json -o $Channel.0.json $Channel.0.xml && rm $Channel.0.xml
+        [ -f $Channel.0.json ] && $PYTHON TVBLOB_EPG.py $Channel.0.json ; rm $Channel.0.json
         wget $WGET_OPT -O $Channel.1.xml http://epgadmin.tvblob.com/api/$Channel/programmes/schedules/tomorrow
-        [ -f $Channel.1.xml  ] && python xml2json.py -t xml2json -o $Channel.1.json $Channel.1.xml && rm $Channel.1.xml 
-        [ -f $Channel.1.json ] && python TVBLOB_EPG.py $Channel.1.json ; rm $Channel.1.json
+        [ -f $Channel.1.xml  ] && $PYTHON xml2json.py -t xml2json -o $Channel.1.json $Channel.1.xml && rm $Channel.1.xml 
+        [ -f $Channel.1.json ] && $PYTHON TVBLOB_EPG.py $Channel.1.json ; rm $Channel.1.json
 
     done
 fi
@@ -64,7 +65,7 @@ then
     echo "Download EPG da CutAndPasta"
     rm -f cnp-epg.xml cnp-epg.json
     wget $WGET_OPT -O cnp-epg.xml http://www.cutandpasta.it/xmltvita/epg.xml
-    [ -f cnp-epg.xml ] && python xml2json.py -t xml2json -o cnp-epg.json cnp-epg.xml && python XMLTV_EPG.py cnp-epg.json
+    [ -f cnp-epg.xml ] && $PYTHON xml2json.py -t xml2json -o cnp-epg.json cnp-epg.xml && $PYTHON XMLTV_EPG.py cnp-epg.json
 fi
 
 #
@@ -105,8 +106,8 @@ then
             rm rytecxmltvItaly.json
             gzip -cd rytecxmltvItaly.gz > rytecxmltvItaly.xml
             touch -r rytecxmltvItaly.gz rytecxmltvItaly.xml
-            python xml2json.py -t xml2json -o  rytecxmltvItaly.json rytecxmltvItaly.xml
-            python XMLTV_EPG.py rytecxmltvItaly.json | tee rytecxmltvItaly.log
+            $PYTHON xml2json.py -t xml2json -o  rytecxmltvItaly.json rytecxmltvItaly.xml
+            $PYTHON XMLTV_EPG.py rytecxmltvItaly.json | tee rytecxmltvItaly.log
         fi
     fi
 fi
