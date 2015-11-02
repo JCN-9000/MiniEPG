6b87c1c20a3346f6203375ef88ca0364c07d68ca
dr-wahrheit <ste.valente@gmail.com>
2015-11-02 17:34:38 +0100

    ~ Ottimizzato la gestione degli alias. Ora il file aliases.json viene letto una sola volta con un discreto aumento di performance.

--- XMLTV_EPG.py
+++ XMLTV_EPG.py
@@ -14,6 +14,7 @@ import json
 
 from unidecode import unidecode
 
+
 def _type(scat):
     """
     Handle categorization
@@ -24,37 +25,31 @@ def _type(scat):
     with open("types.json") as types_file:
         js = json.load(types_file)
 
-    typ=None
+    typ = None
 
     if scat:
         #~ print(scat)
-        if isinstance(scat,list):
+        if isinstance(scat, list):
             cat = scat[0].get("#text")
         else:
             cat = scat.get("#text")
-        cat=cat.split('<')[0]
+        cat = cat.split('<')[0]
 
         xtyp = js["xml_types"].get(cat.title())
         if xtyp:
-            typ=xtyp
+            typ = xtyp
 
         #~ print(typ, cat)
     return typ
 
-def _channel_name(name):
+
+def _channel_name(name, aliases):
     """
     Handle inconsistent spelling of channel names
-    Load external alias file: aliases.json
     """
+    newname = aliases.get(name)
+    return name if not newname else newname
 
-    with open("aliases.json") as alias_file:
-        js = json.load(alias_file)
-
-    newname = js["aliases"].get(name)
-    if not newname:
-        newname=name
-
-    return newname
 
 def _mins_since_epoch(datestring):
     """
@@ -62,12 +57,13 @@ def _mins_since_epoch(datestring):
     """
 #    return int(datetime.datetime.strptime(datestring.split('+')[0],
 #            '%Y%m%d%H%M%S ').strftime("%s")) / 60
-    epoch = datetime.datetime(1970,1,1)
+    epoch = datetime.datetime(1970, 1, 1)
     sometime = datetime.datetime.strptime(datestring.split('+')[0],
-        '%Y%m%d%H%M%S ')
-     
-    return ( sometime - epoch ).total_seconds() / 60
-        
+                                          '%Y%m%d%H%M%S ')
+
+    return (sometime - epoch).total_seconds() / 60
+
+
 def _text(title):
     """
     Extract text from tag element - if there are multiple elements, use first
@@ -85,10 +81,10 @@ def _text(title):
         text = str(title)
     return text
 
-def _CleanupTitle(tit):
 
-    tit = tit.replace('Film ','',1)
-    tit = tit.replace('Telefilm ','',1)
+def _CleanupTitle(tit):
+    tit = tit.replace('Film ', '', 1)
+    tit = tit.replace('Telefilm ', '', 1)
     tit = tit.replace('Xxvii', 'XXVII')
     tit = tit.replace('Xxvi',  'XXVI')
     tit = tit.replace('Xxv',   'XXV')
@@ -109,45 +105,51 @@ def _CleanupTitle(tit):
     tit = tit.replace('Ix',    'IX')
     tit = tit.replace('Viii',  'VIII')
     tit = tit.replace('Vii',   'VII')
-    tit = tit.replace('Vi',    'VI') 
+    tit = tit.replace('Vi',    'VI')
     tit = tit.replace('Iv',    'IV')
     tit = tit.replace('Iii',   'III')
     tit = tit.replace('Ii',    'II')
     tit = tit.replace('Xxi',   'XXI')
-    tit = tit.replace('Tgcom','TGCOM')
-    tit = tit.replace('Tgr','TGR')
-    tit = tit.replace('Tg','TG')
-    tit = tit.replace('Wwe','WWE')
-    tit = tit.replace('Wta','WTA')
-    tit = tit.replace('Mtv','MTV')
+    tit = tit.replace('Tgcom', 'TGCOM')
+    tit = tit.replace('Tgr', 'TGR')
+    tit = tit.replace('Tg', 'TG')
+    tit = tit.replace('Wwe', 'WWE')
+    tit = tit.replace('Wta', 'WTA')
+    tit = tit.replace('Mtv', 'MTV')
 ##    tit = unidecode(tit).replace('\\','')
-    tit = tit.replace('\\','')
+    tit = tit.replace('\\', '')
     return tit
+
+
+def _load_file_as_json(file_):
+    jcontent = None
+    with open(file_) as f:
+        jcontent = json.load(f)
+    return jcontent
 
 
 def main():
     """
     Parse JSON file, update SQLIte DB
     """
-
     filename = sys.argv[1]
-
-    with open(filename) as data_file:
-        jsondata = json.load(data_file)
-
+    jsondata = _load_file_as_json(filename)
     prog = jsondata["tv"].get("programme")
-
     if not prog:
         print("Nessun Programma Disponibile in ", filename)
         sys.exit(1)
 
+    # Load external alias file: aliases.json
+    jaliases = _load_file_as_json('aliases.json')
+    aliases = jaliases['aliases']
+
     conn = sqlite3.connect('epg.v2.sqlite')
     cursor = conn.cursor()
 
-    chskip=()
-    chkeep=()
+    chskip = ()
+    chkeep = ()
     for prog in jsondata["tv"]["programme"]:
-        channel_name = _channel_name(prog["@channel"])
+        channel_name = _channel_name(prog["@channel"], aliases)
 
         # Load existing channel data from DB
         params = (channel_name,)
@@ -156,11 +158,11 @@ def main():
         if not row:
             if channel_name not in chskip:
                 print("Ignoring unrecognised channel  \"{0}\" ".format(channel_name))
-                chskip=channel_name
+                chskip = channel_name
             continue
         if channel_name not in chkeep:
             print("Analisi dati EPG del canale   \"{0}\" ".format(channel_name))
-            chkeep=channel_name
+            chkeep = channel_name
 
         channel = row[0]
 
@@ -175,7 +177,7 @@ def main():
 
         params = (channel, end_date)
         cursor.execute('SELECT id FROM show WHERE channel = ? AND end_date= ?',
-            params)
+                       params)
         row = cursor.fetchone()
         if row:
             #print('Entry for show already present, skipping...')
@@ -198,10 +200,10 @@ def main():
             title = unidecode(line)
             if title[0:4] == u'dvd ':
                 title = title[4:]
-                ptype = 6 # FILM
+                ptype = 6  # FILM
             elif title[0:20] == u'Recensione del film ':
                 title = title[20:]
-                ptype = 6 # FILM
+                ptype = 6  # FILM
             elif title[0:11] == u'Soundtrack ':
                 title = title[11:]
                 ptype = 6
@@ -239,4 +241,3 @@ if __name__ == "__main__":
     main()
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-
