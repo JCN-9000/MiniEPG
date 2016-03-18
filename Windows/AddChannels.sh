f665074db4a0555b78759354e5e8a802a9630be0
Alberto 'JCN-9000 <jcn9000@gmail.com>
2016-03-18 09:29:15 +0100

    Paramount Channel, Sky TG24

--- Windows/AddChannels.sh
+++ Windows/AddChannels.sh
@@ -16,7 +16,6 @@ INSERT INTO channel VALUES(402,'TOPcrime','272.905.2150','',39,0,'Europe/Rome',0
 INSERT INTO channel VALUES(403,'Boing','272.905.145','',40,0,'Europe/Rome',0);
 INSERT INTO channel VALUES(404,'Cartoonito','272.905.147','',46,0,'Europe/Rome',0);
 INSERT INTO channel VALUES(405,'Super!','29.514.106','',47,0,'Europe/Rome',0);
-INSERT INTO channel VALUES(406,'LaEFFE','8572.31000.5','',50,0,'Europe/Rome',0);
 INSERT INTO channel VALUES(407,'TGCOM24','272.940.4014','',51,0,'Europe/Rome',0);
 INSERT INTO channel VALUES(408,'DMAX','29.516.50','',52,0,'Europe/Rome',0);
 INSERT INTO channel VALUES(409,'Focus TV','8572.31000.25','',56,0,'Europe/Rome',0);
@@ -30,6 +29,8 @@ INSERT INTO channel VALUES(416,'Agon','29.516.10','',33,0,'Europe/Rome',0);
 INSERT INTO channel VALUES(417,'RTL 102.5 TV','29.516.21','',36,0,'Europe/Rome',0);
 INSERT INTO channel VALUES(418,'Gazzetta TV','29.514.50','',59,0,'Europe/Rome',0);
 INSERT INTO channel VALUES(419,'Rai Scuola','318.2.8564','',146,0,'Europe/Rome',0);
+INSERT INTO channel VALUES(420,'Paramount Channel','8572.31000.3','',50,0,'Europe/Rome',0);
+INSERT INTO channel VALUES(421,'Sky TG24','8572.31000.5','',50,0,'Europe/Rome',0);
 EOD
 
 $UNZIP $UNZIP_OPT epg.v2.sqlite.zip
